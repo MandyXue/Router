7d9166d2473d440b822e34fe04ee0c83792f6b89
Harold LIU <7lxd@tongji.edu.cn>
2016-05-04 14:17:38 +0800

    Finish View Login part --Liu

--- Router/SignupTableViewController.swift
+++ Router/SignupTableViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class SignupTableViewController: UITableViewController {
     
+    @IBOutlet weak var MailView: UIView!
+    @IBOutlet weak var UsernameView: UIView!
+    @IBOutlet weak var PasswordView: UIView!
+    @IBOutlet weak var ConfirmView: UIView!
+    @IBOutlet weak var SignUpButton: UIButton!
+    
+    
+    @IBOutlet weak var mailText: UITextField!
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var confirmPassword: UITextField!
+    
+    
     // MARK: - BaseViewController
     
     static func loadFromStoryboard() -> UIViewController {
@@ -20,84 +33,27 @@ class SignupTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        configureUI()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func configureUI() {
+        tableView.backgroundView = UIImageView(image:UIImage(named: "background"))
+        MailView.layer.cornerRadius = MailView.bounds.height/2
+        UsernameView.layer.cornerRadius = UsernameView.bounds.height/2
+        PasswordView.layer.cornerRadius = PasswordView.bounds.height/2
+        ConfirmView.layer.cornerRadius = ConfirmView.bounds.height/2
+        UIButton.defaultStyle(SignUpButton)
+        
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    
+    @IBAction func Close() {
+        dismissViewControllerAnimated(true, completion: nil)
     }
-
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    
+    @IBAction func SignUp() {
     }
-
-    /*
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    
+    @IBAction func toLogin() {
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
