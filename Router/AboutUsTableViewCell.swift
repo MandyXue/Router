ff37d38728374a0fe7e0a761edbfa5067e054b59
MandyXue <13564039693@163.com>
2016-05-18 20:33:47 +0800

    setting views

---
+++ Router/AboutUsTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  AboutUsTableViewCell.swift
+//  L-FMS
+//
+//  Created by MandyXue on 16/2/25.
+//  Copyright © 2016年 L&FMS. All rights reserved.
+//
+
+import UIKit
+
+class AboutUsTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var avatar: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var introduction: UILabel!
+    
+}
